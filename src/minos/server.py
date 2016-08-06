56cc729fca81d863223e12110c2c3d88e845c23b
Marcus Brinkmann <marcus.brinkmann@ruhr-uni-bochum.de>
2016-08-07 01:59:22 +0200

    Add experiment interface.

--- src/minos/server.py
+++ src/minos/server.py
@@ -1,8 +1,34 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, abort, g
 
 from . minos import __version__, __program_name__
+from . experiment import Experiment, ExperimentList
 
 app = Flask(__name__)
+
+
+def db_open():
+    if not hasattr(g, 'neo4j_db'):
+        g.neo4j_db = app.minos.driver.session()
+    return g.neo4j_db
+
+@app.teardown_appcontext
+def db_close(error):
+    if hasattr(g, 'neo4j_db'):
+        g.neo4j_db.close()
+
+# curl -XGET localhost:5000/api/experiment/<ID>
+@app.route('/api/experiment/<int:ident>', methods=['GET'])
+def experiment(ident):
+    exp = Experiment(ident)
+    resp = jsonify(exp.export())
+    return resp
+
+# curl -XGET localhost:5000/api/experiment/_list
+@app.route('/api/experiment/_list', methods=['GET'])
+def experiment_list():
+    exps = ExperimentList()
+    resp = jsonify(exps.export())
+    return resp
 
 # curl -XGET localhost:5000/api/status
 @app.route('/api/status', methods=['GET'])
